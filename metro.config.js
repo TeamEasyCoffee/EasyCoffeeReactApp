d00c1a9ff2e9110eeadd7af8c8c40be96edb6d8c
Cuckoo <3241508143@qq.com>
2026-04-01 18:06:24 +0800

    添加 SQLite插件和依赖配置

--- metro.config.js
+++ metro.config.js
@@ -16,4 +16,17 @@ config.resolver = {
   sourceExts: [...resolver.sourceExts, "svg"],
 };
 
+// 网页端sqlite支持
+// Add wasm asset support
+config.resolver.assetExts.push("wasm");
+
+// Add COEP and COOP headers to support SharedArrayBuffer
+config.server.enhanceMiddleware = (middleware) => {
+  return (req, res, next) => {
+    res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    middleware(req, res, next);
+  };
+};
+
 module.exports = withNativeWind(config, { input: "./global.css" });
