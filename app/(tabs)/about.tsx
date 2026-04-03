e2991556355302cead362085130b4e6c7d0cad0d
Cuckoo <3241508143@qq.com>
2026-04-03 14:28:17 +0800

    数据库控制台

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,66 +1,56 @@
-import dbCoffeeDataBase from "@/services/DataBseInit";
-import { useState } from "react";
-import { FlatList, Pressable, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { Text, TextInput, View } from "react-native";
 
+import coffeeSQLite from "@/services/coffeeSQLite.js";
 export default function about() {
-  const [message, setmessage] = useState("aa");
-  let [dataMsg, setDataMsg] = useState("");
-  let [commandDataBase, setCommandDataBase] = useState("");
+    const [userCommand, setUserCommand] = useState("");
+    const [returnMsg, setReturnMsg] = useState("");
+    //
+    async function runCommand() {
+        setReturnMsg(
+            JSON.stringify(await coffeeSQLite.useCommand(userCommand)),
+        );
+    }
 
-  const test = async () => {
-    console.log("test");
-    await dbCoffeeDataBase.initDb();
-    await dbCoffeeDataBase.firstMakeTables();
-    await dbCoffeeDataBase.insertTest();
-    const rows = await dbCoffeeDataBase.selectTableAll("coffee_beans");
-    console.log("查询", rows);
-    setDataMsg(rows);
-  };
+    return (
+        <View>
+            <View className="p-2 DataBaseCli bg-slate-600">
+                <View className="Title">
+                    <Text>数据库控制台</Text>
+                </View>
+                <View className="flex-row items-center p-2">
+                    <Text className="text-white ">{">_"}</Text>
+                    <TextInput
+                        className="m-1 text-white w-70%"
+                        value={userCommand}
+                        placeholder="输入指令..."
+                        onChangeText={setUserCommand}
+                        onSubmitEditing={runCommand}
+                    ></TextInput>
+                </View>
+                <View className="flex-row justify-around">
+                    <Text
+                        onPress={async () => {
+                            setReturnMsg(
+                                JSON.stringify(
+                                    await coffeeSQLite.useCommand(
+                                        "select * from coffee_beans",
+                                    ),
+                                ),
+                            );
+                        }}
+                        className="p-1 text-white rounded-md bg-sky-300"
+                    >
+                        豆仓表
+                    </Text>
 
-  //向数据库发送指令
-  // select * from coffee_beans
-  async function commandToDataBase() {
-    await dbCoffeeDataBase.initDb();
-    console.log(commandDataBase);
-    let backMsg = await dbCoffeeDataBase.sendCommand(commandDataBase);
-    console.log(backMsg);
-    setDataMsg(JSON.stringify(backMsg));
-  }
-  return (
-    <View className="items-center justify-center flex-1 bg-white">
-      <View className="">
-        <View>
-          <Text>数据库:</Text>
+                    <Text></Text>
+                    <Text>豆仓</Text>
+                </View>
+                <View className="returnMsgs">
+                    <Text>{returnMsg ? returnMsg : "回调信息..."}</Text>
+                </View>
+            </View>
         </View>
-        <View>
-          <TextInput
-            value={commandDataBase}
-            onChangeText={setCommandDataBase}
-            className=" border-l-blue-500 border-spacing-1"
-          />
-          <Pressable
-            onPress={commandToDataBase}
-            className=" w-[60px] p-4 text-white rounded-md  bg-slate-600"
-          >
-            <Text>{"=>"}</Text>
-          </Pressable>
-          <Text className=" w-[100vw]">{dataMsg}</Text>
-        </View>
-      </View>
-      <Pressable onPress={test}>
-        <View className="m-3 text-white rounded-md bg-slate-500">
-          <Text>测试数据库</Text>
-        </View>
-      </Pressable>
-      <FlatList
-        data={dataMsg}
-        renderItem={({ item }) => (
-          <View>
-            <Text>{JSON.stringify(item)}</Text>
-          </View>
-        )}
-      ></FlatList>
-      {/* <Text className="text-orange-600 "> {}</Text> */}
-    </View>
-  );
+    );
 }
